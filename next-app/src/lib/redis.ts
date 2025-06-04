4302fade45e62091dcfe598c1df4f60f5dad5941
Franco <sainex1314@gmail.com>
2025-06-04 00:44:13 -0600

    redis dockerizado

--- next-app/src/lib/redis.ts
+++ next-app/src/lib/redis.ts
@@ -1,5 +1,9 @@
-import Redis from "ioredis";
+import Redis from "ioredis"
 
-const redis = new Redis(process.env.REDIS_URL!);
+const redis = new Redis({
+  host: process.env.NODE_ENV === "development" ? "127.0.0.1" : "redis",
+  port: 6379,
+  password: "JJX*EMMqeeh!78QQ!BD" 
+})
 
-export default redis;
+export default redis
